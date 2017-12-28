8585b98f3c0c09091ecf6576abc3b3dee2497fac
sniperjjs <sniperjjs@163.com>
2017-12-28 21:25:18 +0800

    convert

---
+++ likun.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+)
+
+type MyBook struct {
+	Name  string
+	Price float64
+}
+
+func getBook() MyBook {
+	book := MyBook{Name: "go", Price: 99}
+	return book
+}
+
+type What struct {
+}
+
+/**struct => json**/
+func (What) Struct2Josn(book *MyBook) (result string, err error) {
+	mybook, err := json.Marshal(book)
+	if err != nil {
+		return
+	}
+	result = string(mybook)
+	return
+}
+
+/**struct => map[string]interface{}**/
+func Struct2MapInt(book MyBook) map[string]interface{} {
+	mapbook := map[string]interface{}{
+		"Name":  book.Name,
+		"Price": book.Price,
+	}
+	return mapbook
+}
+func Struct2MapInt2(obj interface{}) map[string]interface{} {
+	t := reflect.TypeOf(obj)
+	v := reflect.ValueOf(obj)
+
+	var data = make(map[string]interface{})
+	for i := 0; i < t.NumField(); i++ {
+		data[t.Field(i).Name] = v.Field(i).Interface()
+	}
+	return data
+}
+
+/**map[string]interface{} => struct**/
+func MapIner2Struct(mapbook map[string]interface{}) MyBook {
+	b, _ := json.Marshal(mapbook)
+	ch := make(chan string, 1)
+	go func(c chan string, str string) {
+		c <- str
+	}(ch, string(b))
+	strData := <-ch
+	mb := &MyBook{}
+	err := json.Unmarshal([]byte(strData), &mb)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(err)
+	}
+	return *mb
+}
+
+/**xml=> json**/
+// func Xml2Json((xmlData) byte[]) (result string, err error) {
+// 	data := &MyBook{}
+// 	err := xml.Unmarshal(xmlData, data)
+// 	if nil != err {
+// 		fmt.Println("Error unmarshalling from XML", err)
+// 		return
+// 	}
+// 	result, err := json.Marshal(data)
+// 	if nil != err {
+// 		fmt.Println("Error marshalling to JSON", err)
+// 		return
+// 	}
+// 	fmt.Printf("%v+\n", result)
+// }
---
+++ likun_test.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"testing"
+
+	"github.com/relax-space/go-kit/test"
+)
+
+func Test_Struct2Josn(t *testing.T) {
+	book := getBook()
+	result, err := What{}.Struct2Josn(&book)
+	fmt.Printf("%+v\n", result)
+	test.Ok(t, err)
+}
+
+func Test_Struct2MapInt(t *testing.T) {
+	book := getBook()
+	mapbook := Struct2MapInt(book)
+	fmt.Printf("%+v\n", mapbook)
+	test.Ok(t, nil)
+}
+
+func Test_Struct2MapInt2(t *testing.T) {
+	book := getBook()
+	mapbook := Struct2MapInt2(book)
+	fmt.Printf("%+v\n", mapbook)
+}
+
+func Test_MapIner2Struct(t *testing.T) {
+	mapbook := map[string]interface{}{
+		"Name":  "go",
+		"Price": 99,
+	}
+	book := MapIner2Struct(mapbook)
+	fmt.Printf("%+v\n", book)
+}
+
+/**xml=> json**/
+func Test_Xml2Json(t *testing.T) {
+	xmlData := []byte(`<?xml version="1.0" encoding="UTF-8" ?>
+    <MyBook>
+        <Name>go</Name>
+        <Price>99</Price>
+    </MyBook>`)
+	data := &MyBook{}
+	err := xml.Unmarshal(xmlData, data)
+	if nil != err {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("%+v\n", data)
+}
+
+/**xml => struct**/
+func Test_Xml2Struct(t *testing.T) {
+	xmlData := []byte(`<?xml version="1.0" encoding="UTF-8" ?>
+		<MyBook>
+			<Name>go</Name>
+			<Price>99</Price>
+		</MyBook>`)
+	data := &MyBook{}
+	err := xml.Unmarshal(xmlData, data)
+	if nil != err {
+		fmt.Println(err)
+		return
+	}
+	b, _ := json.Marshal(data)
+	ch := make(chan string, 1)
+	go func(c chan string, str string) {
+		c <- str
+	}(ch, string(b))
+	strData := <-ch
+	mb := &MyBook{}
+	err1 := json.Unmarshal([]byte(strData), &mb)
+	if nil != err1 {
+		fmt.Println(err1)
+		return
+	}
+	fmt.Printf("%+v\n", *mb)
+}
+
+/**xml=>map[string]interface{}**/
+func Test_Xml2Map(t *testing.T) {
+	xmlData := []byte(`<?xml version="1.0" encoding="UTF-8" ?>
+		<MyBook>
+			<Name>go</Name>
+			<Price>99</Price>
+		</MyBook>`)
+	data := &MyBook{}
+	err := xml.Unmarshal(xmlData, data)
+	if nil != err {
+		fmt.Println(err)
+		return
+	}
+	mapbook := Struct2MapInt(*data)
+	fmt.Printf("%+v\n", mapbook)
+}
+
+/**struct => xml**/
+func Test_Strut2Xml(t *testing.T) {
+	book := getBook()
+	output, err := xml.MarshalIndent(book, "  ", "    ")
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+	}
+	str_output := string(output)
+	fmt.Printf("%+v\n", str_output)
+}
